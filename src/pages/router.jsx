f68f910c11b8abc2a4ec82173080ecc5b8df5d15
jun060703 <jun00060007@gmail.com>
2023-05-08 14:59:08 +0900

    react

---
+++ src/pages/router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <routes>
+        <Route />
+      </routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
