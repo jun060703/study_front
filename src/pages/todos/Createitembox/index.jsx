55c554e175b2132c051535538605ae36583626f4
jun060703 <jun00060007@gmail.com>
2023-05-11 10:36:40 +0900

    keyDown

--- src/pages/todos/Createitembox/index.jsx
+++ src/pages/todos/Createitembox/index.jsx
@@ -5,10 +5,18 @@ function CreateItemBox({ value, onChange, createTodo }) {
   const handleInputValue = e => {
     onChange(e.target.value);
   };
-
+  const onKeyPress = e => {
+    if (e.key == 'Enter') {
+      createTodo();
+    }
+  };
   return (
     <S.InputBox>
-      <S.Input value={value} placeholder='입력' onChange={handleInputValue}></S.Input>
+      <S.Input
+        value={value}
+        placeholder='입력'
+        onChange={handleInputValue}
+        onKeyDown={onKeyPress}></S.Input>
       <Button onClick={createTodo}>등록</Button>
     </S.InputBox>
   );
