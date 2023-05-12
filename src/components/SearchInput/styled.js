b82ff6c4545a7472b515aac2ff5f4a3a7a52b378
jun060703 <jun00060007@gmail.com>
2023-05-12 10:21:41 +0900

    style추가

---
+++ src/components/SearchInput/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SearchInputBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+`;
