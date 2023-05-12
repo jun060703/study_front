7c429e950b4f429652a216e88e4744b92eedacf6
jun060703 <jun00060007@gmail.com>
2023-05-12 10:22:30 +0900

    추가

--- src/pages/todos/ItemList/index.jsx
+++ src/pages/todos/ItemList/index.jsx
@@ -1,18 +1,21 @@
 import * as S from './styled';
 import Todo from '../../../components/todo';
-function ItemList({ todos, deleteTodo }) {
+function ItemList({ todos, searchvalue, deleteTodo }) {
   return (
     <S.ItemList>
-      {todos.map(({ id, name }) => {
-        const handleDeleteTodo = () => {
-          deleteTodo(id);
-        };
-        return (
-          <Todo key={id} deleteTodo={handleDeleteTodo}>
-            {name}
-          </Todo>
-        );
-      })}
+      {todos
+        //filter함수
+        .filter(({ todo }) => todo.incluedes(searchvalue))
+        .map(({ id, name }) => {
+          const handleDeleteTodo = () => {
+            deleteTodo(id);
+          };
+          return (
+            <Todo key={id} deleteTodo={handleDeleteTodo}>
+              {name}
+            </Todo>
+          );
+        })}
     </S.ItemList>
   );
 }
