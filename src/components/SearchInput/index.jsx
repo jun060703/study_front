a1c58fff6adc7121907175831ecc8713e3aeb516
jun060703 <jun00060007@gmail.com>
2023-05-12 10:21:57 +0900

    new

---
+++ src/components/SearchInput/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import * as S from './styled';
+function SearchInput({ onChange }) {
+  const [value, setValue] = useState('');
+
+  const handleValue = ({ target: { value } }) => {
+    setValue(value);
+    onChange?.(value);
+  };
+
+  return (
+    <S.SearchInputBox>
+      <S.SearchInput plcaeholder='검색할 내용을 입력하세요' value={value} onChange={handleValue} />
+    </S.SearchInputBox>
+  );
+}
+export default SearchInput;
